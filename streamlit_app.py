601c4a72b5a110e35d92822b1e49d9f483471476
andrilblaze40 <andrilizu35@gmail.com>
2025-08-17 10:08:25 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,39 @@ with st.expander("Data"):
   df= pd.read_csv("cleaned_breast_cancer_data.csv")
   df['diagnosis'].replace({'M':1,'B':0},inplace=True)
   df
+
+# Input features
+with st.sidebar:
+  st.header('Input features')
   
+  texture_worst = st.slider('texture worse', -2.7, 2.7	, -0.0)
+  texture_se = st.slider('texture_se', -2.6, 2.6, -0.0)
+  texture_mean = st.slider('texture_mean', -6.8	, 6.0, -0.5)
+  symmetry_worst= st.slider('symmetry_worst', -2.7, -2.7, 0.0) 
+  symmetry_se= st.slider('symmetry_se', -6.0, 6.3, -0.0)
+  symmetry_mean= st.slider('symmetry_mean', -7.2, 6.4, -0.4)
+  smoothness_worst= st.slider('smoothness_worst', -11.9	, 11.7, -0.1)
+  radius_worst= st.slider('radius_worst', -2.7, 2.7, -0.7)
+  smoothness_se= st.slider('smoothness_se', -4.6, 6.6, 1.0)
+  smoothness_mean= st.slider('smoothness_mean', -13.2, 10.3, -1.5)
+
+   
+  # Create a DataFrame for the input features
+  data = {'texture_worst': texture_worst,
+          'texture_se': texture_se,
+          'texture_mean': texture_mean,
+          'symmetry_worst': symmetry_worst,
+          'symmetry_se': symmetry_se,
+          'symmetry_mean': symmetry_mean,
+          'smoothness_worst': smoothness_worst,
+          'radius_worst': radius_worst,
+          'smoothness_se': smoothness_se,
+          'smoothness_mean': smoothness_mean}
+  input_df = pd.DataFrame(data, index=[0])
+  input_data = pd.concat([input_df, X], axis=0)
+
+
+
     
     
   
