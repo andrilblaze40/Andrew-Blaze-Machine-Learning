cb5742535e1558ac3207338f2de9498404e475a9
andrilblaze40 <andrilizu35@gmail.com>
2025-08-17 09:20:36 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,19 @@
 import streamlit as st
+import pandas as pd
+from sklearn.base import ClassifierMixin
+from sklearn.pipeline import Pipeline
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.impute import SimpleImputer
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+from sklearn.model_selection import GridSearchCV, cross_val_score, train_test_split
+from sklearn.pipeline import make_pipeline
+import seaborn as sns
+from imblearn.over_sampling import RandomOverSampler
 
-st.title('🎈 App Name')
 
-st.write('Hello world!')
+
+st.title('🎈ML CANCER DIAGNOSIS APP')
+
+st.info('MACHINE LEARNING PIPELINE')
